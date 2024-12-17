38492c0d36e1be3c58c3744bc8c1066c47ce52c1
Alejandro Villegas <alex.ansi.c@gmail.com>
2024-12-17 13:32:49 +0100

    fix: Fixed and improved route53 module for getting consoleURL of clusters
    Signed-off-by: Alejandro Villegas <alex.ansi.c@gmail.com>

--- go.mod
+++ go.mod
@@ -1,14 +1,14 @@
 module github.com/RHEcosystemAppEng/cluster-iq
 
-go 1.19
+go 1.21
 
 require (
-	github.com/aws/aws-sdk-go v1.44.265
+	github.com/aws/aws-sdk-go v1.55.5
+	github.com/aws/aws-sdk-go-v2/service/route53 v1.46.3
 	github.com/gin-contrib/zap v0.1.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.9
-	github.com/redis/go-redis/v9 v9.0.4
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.3.2
 	github.com/swaggo/swag v1.16.2
@@ -18,11 +18,12 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.32.6 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.25 // indirect
+	github.com/aws/smithy-go v1.22.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.0 // indirect
@@ -44,12 +45,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
-	github.com/urfave/cli/v2 v2.25.7 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.opentelemetry.io/otel v1.10.0 // indirect
 	go.opentelemetry.io/otel/trace v1.10.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
@@ -60,7 +57,5 @@ require (
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
---
+++ internal/stocker/aws_stocker_tags.go
@@ -0,0 +1,80 @@
+package stocker
+
+import (
+	"strings"
+
+	"github.com/RHEcosystemAppEng/cluster-iq/internal/inventory"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+// getInfraIDFromTags search and return the infrastructure associted to the instance,
+// if it belongs to a cluster. Empty string is returned if the
+// instance doesn't belong to any cluster
+func getInfraIDFromTags(instance ec2.Instance) string {
+	var tags []ec2.Tag
+	tags = *(lookForTagByValue("owned", instance.Tags))
+	for _, tag := range tags {
+		if strings.Contains(*tag.Key, inventory.ClusterTagKey) {
+			return parseInfraID(*(tag.Key))
+		}
+	}
+	return ""
+}
+
+// getInstanceNameFromTags search and return the instance's name based on its tags.
+func getInstanceNameFromTags(instance ec2.Instance) string {
+	var tags []ec2.Tag
+	tags = *(lookForTagByKey("Name", instance.Tags))
+	if len(tags) == 1 {
+		return *(tags[0].Value)
+	} else {
+		return ""
+	}
+}
+
+// getOwnerFromTags search and return the instance's Owner based on its tags.
+func getOwnerFromTags(instance ec2.Instance) string {
+	var tags []ec2.Tag
+	tags = *(lookForTagByKey("Owner", instance.Tags))
+	if len(tags) == 1 {
+		return *(tags[0].Value)
+	} else {
+		return ""
+	}
+}
+
+// getClusterTag search and return the cluster name associted to the instance,
+// if it belongs to a cluster. UnknownClusterNameCode is returned if the
+// instance doesn't belong to any cluster
+func getClusterNameFromTags(instance ec2.Instance) string {
+	var tags []ec2.Tag
+	tags = *(lookForTagByValue("owned", instance.Tags))
+	for _, tag := range tags {
+		if strings.Contains(*tag.Key, inventory.ClusterTagKey) {
+			return parseClusterName(*(tag.Key))
+		}
+	}
+	return unknownClusterNameCode
+}
+
+// lookForTagByValue returns an array of ec2.Tag with every tag found with the specified value
+func lookForTagByValue(value string, tags []*ec2.Tag) *[]ec2.Tag {
+	var resultTags []ec2.Tag
+	for _, tag := range tags {
+		if *tag.Value == value {
+			resultTags = append(resultTags, *tag)
+		}
+	}
+	return &resultTags
+}
+
+// lookForTagByKey returns an array of ec2.Tag with every tag found with the specified key
+func lookForTagByKey(key string, tags []*ec2.Tag) *[]ec2.Tag {
+	var resultTags []ec2.Tag
+	for _, tag := range tags {
+		if *tag.Key == key {
+			resultTags = append(resultTags, *tag)
+		}
+	}
+	return &resultTags
+}
