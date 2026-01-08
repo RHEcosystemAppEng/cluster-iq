beca047cf3632d80f5199e37241b618c9b54073e
r2dedios <alex.ansi.c@gmail.com>
2026-01-08 19:41:49 +0100

    fix: added missing bracket and skiping error handling
    Signed-off-by: r2dedios <alex.ansi.c@gmail.com>

--- internal/stocker/aws_stocker_ec2.go
+++ internal/stocker/aws_stocker_ec2.go
@@ -58,17 +58,17 @@ func (s *AWSStocker) processInstances(instances []inventory.Instance) {
 				continue
 			}
 
-		if !s.Account.IsClusterInAccount(cluster.ClusterID) {
-			s.Account.AddCluster(cluster)
-		}
+			if !s.Account.IsClusterInAccount(cluster.ClusterID) {
+				_ = s.Account.AddCluster(cluster)
+			}
 
-		if err := s.Account.Clusters[clusterID].AddInstance(&instance); err != nil {
-			s.logger.Error("error adding instance to cluster during instance processing",
-				zap.String("account_id", s.Account.AccountID),
-				zap.String("cluster_id", clusterID),
-				zap.String("instance_id", instance.InstanceID),
-				zap.Error(err),
-			)
+			if err := s.Account.Clusters[clusterID].AddInstance(&instance); err != nil {
+				s.logger.Error("error adding instance to cluster during instance processing",
+					zap.String("account_id", s.Account.AccountID),
+					zap.String("cluster_id", clusterID),
+					zap.String("instance_id", instance.InstanceID),
+					zap.Error(err))
+			}
 		}
 	}
 }
